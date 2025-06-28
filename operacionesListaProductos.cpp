41a42f2a167a6d4c017d3f1b6d6b1cf54f7142ca
CrazierMars <rriverad@ucenfotec.ac.cr>
2025-06-28 10:47:36 -0600

    Actualizacion de operacionesProducto y operacionesListaProductos cpp

--- operacionesListaProductos.cpp
+++ operacionesListaProductos.cpp
@@ -1,1 +1,92 @@
-//pendiente
+#include "listaProductos.h"
+#include <iostream>
+using namespace std;
+
+// getters
+producto* listaProductos::getPrimero() {
+    return primero;
+}
+
+producto* listaProductos::getUltimo(){
+    producto* actual = primero; 
+    if(actual == nullptr) {
+        cout << "La Lista esta vacia" << endl;
+    }
+    while(actual ->getSiguiente() != nullptr) {
+        actual = actual->getSiguiente();
+    }
+    return actual;
+}
+
+// Buscar productos por id, nombre o precio
+producto* listaProductos::buscarProductoId(int pId) {
+    producto* actual = primero;
+    while (actual != nullptr){
+        if (actual->getId() == pId){
+            return actual;
+        }
+        actual = actual->getSiguiente();
+    }
+    return nullptr;
+    cout << "Producto con ID " << pId << " no encontrado." << endl;
+}
+producto* listaProductos::buscarProductoNombre(const std::string& pNombre) {
+    producto* actual = primero;
+    while (actual != nullptr){
+        if (actual->getNombre() == pNombre){
+            return actual;
+        }
+        actual = actual->getSiguiente();
+    }
+    return nullptr;
+    cout << "Producto con nombre " << pNombre << "no encontrado." << endl;
+}
+producto* listaProductos::buscarProductoPrecio(double pPrecio){
+    producto* actual = primero; 
+    while(actual != nullptr){
+        if(actual->getPrecio() == pPrecio){
+            return actual;
+        }
+        actual = actual->getSiguiente();
+    }
+    return nullptr; 
+    cout << "Producto con precio " << pPrecio << "no encontrado." << endl;
+}
+
+// Metodos de insercion
+void listaProductos::agregarProductoInicio(int pNuevoId, const std::string& pNuevoNombre, double pNuevoPrecio, int pNuevaCantidad) {
+    producto* nuevoProducto = new producto(pNuevoId, pNuevoNombre, pNuevoPrecio, pNuevaCantidad);
+    nuevoProducto->setSiguiente(primero);
+    primero = nuevoProducto;
+}
+void listaProductos::agregarProductoFinal(int pNuevoId, const std::string& pNuevoNombre, double pNuevoPrecio, int pNuevaCantidad) {
+    producto* nuevoProducto = new producto(pNuevoId, pNuevoNombre, pNuevoPrecio, pNuevaCantidad);
+    if(primero == nullptr) {
+        primero = nuevoProducto;
+    } else {
+        producto* ultimo = this->getUltimo();
+        ultimo->setSiguiente(nuevoProducto);
+    }
+}
+//
+void listaProductos::imprimirLista() {
+    producto* actual = primero; 
+    if (primero == nullptr) {
+        cout << "La lista esta vacia." << endl;
+        return;
+    }while(actual != nullptr) {
+        cout << "ID: " << actual->getId() << ", Nombre: " << actual->getNombre() 
+             << ", Precio: " << actual->getPrecio() << ", Cantidad: " << actual->getCantidad() << endl;
+        actual = actual->getSiguiente();
+    }
+}
+//cantidad de productos en la lista
+int listaProductos::contarProductos() {
+    int contador = 0;
+    producto* actual = primero; 
+    while (actual != nullptr){
+        contador++;
+        actual = actual->getSiguiente();
+    }
+    return contador;
+}
--- operacionesProducto.cpp
+++ operacionesProducto.cpp
@@ -1,1 +1,36 @@
-//pendiente
+#include "producto.h"
+
+// Getters
+int producto::getId(){
+    return id;
+}
+//Revisar esto
+std::string producto::getNombre(){
+    return nombre;
+}
+double producto::getPrecio(){
+    return precio;
+}
+int producto::getCantidad(){
+    return cantidad;
+}
+producto* producto::getSiguiente(){
+    return siguiente;
+}
+
+// Setters 
+void producto::setId(int pId){
+    id = pId;
+}
+void producto::setNombre(const std::string& pNombre){
+    nombre = pNombre;
+}
+void producto::setPrecio(double pPrecio){
+    precio = pPrecio;
+}
+void producto::setCantidad(int pCantidad){
+    cantidad = pCantidad;
+}
+void producto::setSiguiente(producto* nuevoSiguiente){
+    siguiente = nuevoSiguiente;
+}
